6f30899201437c32117c3b3900c6df1ceacc1cb3
ana <ana@email.com>
2021-02-01 22:44:43 -0300

    Avanço no curso de JavaScript 2

--- formacao front_end/2. JavaScript/2 - Programando a Orientação a Objetos/ContaCorrente.js
+++ formacao front_end/2. JavaScript/2 - Programando a Orientação a Objetos/ContaCorrente.js
@@ -1,9 +1,14 @@
 export class ContaCorrente {
+    //Atributos
     agencia;
-    _cliente;
 
+    _cliente;
+    _saldo = 0; /*adicionado a # no código para que ele seja *privado*. Ou seja, só pode ser alterado por operações da classe
+    definir inicialmente como 0, para que ele tenha um valor inicial */
+
+    //Acessores
     set cliente(novoValor) { //set = acessor de atribuição
-        if(novoValor instanceof Cliente){ //só aceita alteração dentro de _cliente, se a "instância" for do tipo "Cliente"
+        if (novoValor instanceof Cliente) { //só aceita alteração dentro de _cliente, se a "instância" for do tipo "Cliente"
             //proteção para que seja atribuindo somente algo que esperamos.
             this._cliente = novoValor;
         }
@@ -13,30 +18,34 @@ export class ContaCorrente {
         return this._cliente;
     }
 
-    _saldo = 0; /*adicionado a # no código para que ele seja *privado*. Ou seja, só pode ser alterado por operações da classe
-    definir inicialmente como 0, para que ele tenha um valor inicial */
-
     get saldo() { //atribui ao saldo somente a propriedade de leitura, ou seja, "pegar" o valor, mas não atribuir um novo valor.
         return this._saldo;
     }
 
-    sacar (valor) {  //*comportamento* de saque. encapsulado dentro da classe
+    constructor(cliente, agencia) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+    }
+
+
+    //Comportamentos
+    sacar(valor) {  //*comportamento* de saque. encapsulado dentro da classe
         if (this._saldo >= valor) { //this = variável que estamos trabalhando no momento, de maneira gerérica.
             this._saldo -= valor;
             return valor; //somente retornando é que uma função utilizando o "sacar" vai ter um conteúdo "disponível". Caso contrário, seria "undefined".
-        }       
+        }
     }
 
-    depositar (valor) { //"{" abre o escopo do . "valor" é *parâmetro* (ou argumento) e "depositar" é *método (ou função)*
+    depositar(valor) { //"{" abre o escopo do . "valor" é *parâmetro* (ou argumento) e "depositar" é *método (ou função)*
         if (valor <= 0) { //cláusulas de proteção. 
-             return; /*Early return>: encerrando a operação para que se a clásula for verdadeira, não continua.
+            return; /*Early return>: encerrando a operação para que se a clásula for verdadeira, não continua.
              Dessa forma o código fica mais legível. Caso ele ficasse muito longo, com ifs dentro de ifs, a operação final ficaria muito longe, a direita. */
         }
 
         this._saldo += valor;
     }
 
-    transferir (valor, conta) { //pedaço de código com "nível de abstração mais alto": utiliza vocabulário de negócio, não de programação.
+    transferir(valor, conta) { //pedaço de código com "nível de abstração mais alto": utiliza vocabulário de negócio, não de programação.
         conta.cidade = "São Paulo"; //adicionando dinamicamente uma variável "cidade", sem precisar declarar ela anteriormente.
         const valorSacado = this.sacar(valor); //this = chama o método para minha própria classe.
     }
--- formacao front_end/2. JavaScript/2 - Programando a Orientação a Objetos/index.js
+++ formacao front_end/2. JavaScript/2 - Programando a Orientação a Objetos/index.js
@@ -1,38 +1,32 @@
-import {Cliente} from "./Cliente.js" //caminho relativo. Em alguns casos é bom utilizar o caminho completo do arquivo.
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js" //caminho relativo. Em alguns casos é bom utilizar o caminho completo do arquivo.
+import { ContaCorrente } from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
+const cliente1 = new Cliente("Renan", 11122233304);
+/* Retirado do código, utilizado agora dentro do constructor Cliete(...)
 cliente1.nome = "Renan";
-cliente1.cpf = 11122233309;
+cliente1.cpf = 11122233309; */
 
-const cliente2 = new Cliente();
-cliente2.nome = "Gra";
-cliente2.cpf = 88822233309;
+const cliente2 = new Cliente("Gra", 88822233309);
 
-const contaCorrenteRenan = new ContaCorrente();
+const contaCorrenteRenan = new ContaCorrente(cliente1, 1001);
 /*contaCorrenteRenan.saldo = 0; -- linha retirada do código. Precisamos proteger o saldo para que não haja interferência direta nela,
 sem que passe pelos padrões de comportamento que criamos dentro da classe. */
+
+/*Linhas retiradas, utilizando agora o construtor new ContaCorrente(...) acima
 contaCorrenteRenan.agencia = 1001;
-contaCorrenteRenan.cliente = cliente1;
+contaCorrenteRenan.cliente = cliente1; */
 contaCorrenteRenan.depositar(500);
 
-const contaCorrenteGra = new ContaCorrente();
-contaCorrenteGra.cliente = cliente2;
-contaCorrenteGra.agencia = 102;
+const contaCorrenteGra = new ContaCorrente(cliente2, 102);
 
 /* Alterativa (não indicada) de definição de elementos: (ao invez de definir nas linhas 8-10)
 contaCorrenteGra.cliente = new Cliente ();
 contaCorrenteGra.cliente.nome = "Gra";
-contaCorrente.cliente.CPF = 88822233309;
-*/
+contaCorrente.cliente.cpf = 88822233309;*/
 
-
-contaCorrenteRenan.transferir(200, contaCorrenteGra);
-
-console.log(contaCorrenteRenan);
-console.log(contaCorrenteGra);
+let valor = 200;
+contaCorrenteRenan.transferir(valor, contaCorrenteGra);
 
 /* demonstativo de valor sacado
 const valorSacado = contaCorrenteRenan.sacar(50);
 console.log("O valor sacado é: " + valorSacado); */
-
